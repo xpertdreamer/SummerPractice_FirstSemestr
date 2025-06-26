0f37529fc9bf7d91ba665aad140961ce10fdccaf
Gurin <galex0707@mail.ru>
2025-06-26 19:05:05 +0300

    Добавлена возможность пользователю ввести длину массива

--- SummerPractice_FirstSemestr/main.c
+++ SummerPractice_FirstSemestr/main.c
@@ -1,11 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "io.h"
 #include "utils.h"
 #include "insertionSort.h"
 #include <locale.h>
 
 #define MAX_FILENAME_LENGTH 256
+#define MAX_RANDOM_VALUE 1000
 
 void removeNewline(char* str) {
     size_t len = strlen(str);
@@ -14,9 +17,27 @@ void removeNewline(char* str) {
     }
 }
 
+void generateRandomArrayToFile(const char* filename, int size) {
+    FILE* file = fopen(filename, "w");
+    if (file == NULL) {
+        fprintf(stderr, "Ошибка создания файла '%s'\n", filename);
+        exit(1);
+    }
+
+    fprintf(file, "%d\n", size); // Записываем размер массива в первую строку
+
+    srand(time(NULL)); // Инициализация генератора случайных чисел
+    for (int i = 0; i < size; i++) {
+        fprintf(file, "%d ", rand() % MAX_RANDOM_VALUE); // Числа от 0 до MAX_RANDOM_VALUE-1
+    }
+
+    fclose(file);
+}
+
 int main() {
     setlocale(LC_ALL, "Ru");
-    //Заставка
+
+    // Заставка
     printf("Учебная практика\n");
     printf("Задание: сортировка вставками\n");
     printf("Номер бригады: 4\n");
@@ -25,6 +46,15 @@ int main() {
 
     char inputFile[MAX_FILENAME_LENGTH];
     char outputFile[MAX_FILENAME_LENGTH];
+    int arraySize;
+
+    // Ввод размера массива
+    printf("Введите размер массива: ");
+    if (scanf("%d", &arraySize) != 1 || arraySize <= 0) {
+        fprintf(stderr, "Некорректный размер массива\n");
+        return 1;
+    }
+    while (getchar() != '\n'); // Очистка буфера ввода
 
     // Ввод имени исходного файла
     printf("Введите имя исходного файла: ");
@@ -41,6 +71,10 @@ int main() {
         return 1;
     }
     removeNewline(outputFile);
+
+    // Генерация случайного массива в файл
+    generateRandomArrayToFile(inputFile, arraySize);
+    printf("Сгенерирован массив из %d случайных чисел в файле '%s'\n", arraySize, inputFile);
 
     int* array = NULL;
     int qt = 0;
